8105883c0112a00e4aa4ac4225b2bb88b6c102db
Colin Kennedy <colinvfx@gmail.com>
2019-04-21 23:40:59 -0700

    Added variant_set_production_shot/usda folder and WIP python representation

---
+++ variant_set_production_shot/python/shot.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# IMPORT THIRD-PARTY LIBRARIES
+from pxr import Usd
+
+
+def main():
+    '''Run the main execution of the current script.'''
+    raise NotImplementedError('Need to write this')
+
+
+if __name__ == '__main__':
+    main()
