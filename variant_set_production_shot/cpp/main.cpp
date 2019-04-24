dd9ba760c30e621fd5cb8a670ac557353f3c935f
Colin Kennedy <colinvfx@gmail.com>
2019-04-24 00:32:39 -0700

    variant_set_production_shot - Got the C++ version to work

--- variant_set_production_shot/cpp/main.cpp
+++ variant_set_production_shot/cpp/main.cpp
@@ -4,12 +4,14 @@
 // IMPORT THIRD-PARTY LIBRARIES
 #include "pxr/usd/sdf/schema.h"
 #include "pxr/usd/usd/stage.h"
+#include "pxr/usd/usd/editContext.h"
+#include "pxr/usd/usd/variantSets.h"
 #include "pxr/usd/usdGeom/sphere.h"
 
 
 char const ASSET_PATH[] = "/tmp/asset.usda";
 char const SEQUENCE_PATH[] = "/tmp/sequence.usda";
-char const SHOT_PATH[] = "/tmp/shot.usda";
+char const SHOT_PATH[] = "/tmp/shot_v001.usda";
 
 
 std::string create_asset() {
@@ -24,23 +26,68 @@ std::string create_asset() {
 }
 
 
-std::string create_sequence() {
+std::string create_sequence(std::string const &asset) {
     auto stage = pxr::UsdStage::CreateNew(SEQUENCE_PATH);
     auto prim = stage->OverridePrim(pxr::SdfPath("/SomeSphere"));
 
-    stage->GetRootLayer()->SetDocumentation(
+    auto root = stage->GetRootLayer();
+    root->InsertSubLayerPath(asset);
+
+    root->SetDocumentation(
         "A common set of data for an entire sequence.");
     stage->SetMetadata(
-        pxr::SdfFieldKeys->Comment, "We override the character to make it bigger and add some viewing option.");
+        pxr::SdfFieldKeys->Comment,
+        "We override the character to make it bigger and add some viewing option."
+    );
+
+    auto variants = prim.GetVariantSets();
+    auto variant = variants.AddVariantSet("some_variant_set");
+    variant.AddVariant("variant_name_1");
+
+    variant.SetVariantSelection("variant_name_1");
+    auto sphere = pxr::UsdGeomSphere(prim);
+
+    {
+        pxr::UsdEditContext context {variant.GetVariantEditContext()};
+        pxr::VtArray<pxr::GfVec3f> color {{1, 0, 0}};
+        sphere.GetDisplayColorAttr().Set(color);
+    }
 
     stage->Save();
 
     return std::string(SEQUENCE_PATH);
 }
 
+
+std::string create_shot(std::string const &sequence) {
+    auto stage = pxr::UsdStage::CreateNew(SHOT_PATH);
+    auto prim = stage->OverridePrim(pxr::SdfPath("/SomeSphere"));
+
+    stage->GetRootLayer()->InsertSubLayerPath(sequence);
+    stage->GetRootLayer()->SetDocumentation("This shot takes the common settings from sequence.usda and adds to them.");
+
+    auto variant = prim.GetVariantSet("some_variant_set");
+
+    variant.AddVariant("variant_name_2");
+    variant.SetVariantSelection("variant_name_2");
+
+    auto sphere = pxr::UsdGeomSphere(prim);
+    {
+        pxr::UsdEditContext context {variant.GetVariantEditContext()};
+        pxr::VtArray<pxr::GfVec3f> color {{0, 1, 0}};
+        sphere.GetDisplayColorAttr().Set(color);
+    }
+
+    stage->Save();
+
+    return SHOT_PATH;
+}
+
+
 int main() {
     auto asset = create_asset();
-    auto sequence = create_sequence();
+    auto sequence = create_sequence(asset);
+    create_shot(sequence);
 
     return 0;
 }
