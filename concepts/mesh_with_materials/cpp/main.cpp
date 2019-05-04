671aed98c021549b93b9405586a2007a8fac6e59
Colin Kennedy <colinvfx@gmail.com>
2019-05-04 15:25:51 -0700

    mesh_with_material - cpp WIP commit

--- concepts/mesh_with_materials/cpp/main.cpp
+++ concepts/mesh_with_materials/cpp/main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 
 // IMPORT THIRD-PARTY LIBRARIES
+#include "pxr/usd/usdShade/material.h"
 #include <pxr/base/tf/token.h>
 #include <pxr/usd/kind/registry.h>
 #include <pxr/usd/sdf/types.h>
@@ -13,28 +14,60 @@
 #include <pxr/usd/usdGeom/xform.h>
 
 
-int attach_billboard(
-    pxr::UsdStageRefPtr &stage,
-    std::string const &root,
-    std::string const &name="card"
-) {
-    auto billboard = pxr::UsdGeomMesh::Define(
-        stage,
-        pxr::SdfPath(root + "/" + name)
-    );
+pxr::UsdGeomMesh attach_billboard(pxr::UsdStageRefPtr &stage,
+                                  std::string const &root,
+                                  std::string const &name = "card") {
+    auto billboard =
+        pxr::UsdGeomMesh::Define(stage, pxr::SdfPath(root + "/" + name));
 
-    billboard.CreatePointsAttr(pxr::VtValue());
-    billboard.CreateFaceVertexCountsAttr(pxr::VtValue());
-    billboard.CreateFaceVertexIndicesAttr(pxr::VtValue());
-    billboard.CreateExtentAttr(pxr::VtValue());
-    auto coordinates = billboard.CreatePrimvar(
-        "st",
-        pxr::SdfValueTypeNames::TexCoord2fArray,
-        pxr::UsdGeomTokens->varying
+    // billboard.CreatePointsAttr(pxr::VtValue());
+    // billboard.CreateFaceVertexCountsAttr(pxr::VtValue());
+    // billboard.CreateFaceVertexIndicesAttr(pxr::VtValue());
+    // billboard.CreateExtentAttr(pxr::VtValue());
+    //
+    // auto coordinates = billboard.CreatePrimvar(
+    //     pxr::TfToken("st"),
+    //     pxr::SdfValueTypeNames->TexCoord2fArray,
+    //     pxr::UsdGeomTokens->varying
+    // );
 
-    )
+    // coordinates.Set();
+
+    return billboard;
+}
+
+
+int attach_surface_shader(
+        pxr::UsdStageRefPtr &stage,
+        pxr::UsdShadeMaterial material,
+        std::string const &root,
+)
+{
+    // shader = UsdShade.Shader.Define(stage, path)
+    // shader.CreateIdAttr("UsdPreviewSurface")
+    // shader.CreateInput("roughness", Sdf.ValueTypeNames.Float).Set(0.4)
+    // shader.CreateInput("metallic", Sdf.ValueTypeNames.Float).Set(0.0)
+    //
+    // material.CreateSurfaceOutput().ConnectToSource(shader, "surface")
+    //
+    // return shader
 
     return 0;
+}
+
+
+int attach_texture(
+        pxr::UsdStageRefPtr &stage,
+        pxr::UsdShadeMaterial material,
+        std::string const &root,
+        std::string const reader_name="stdReader",
+        std::string const shader_name="diffuseTexture",
+)
+{
+    auto reader = pxr::UsdShadeShader::Define(stage, pxr::SdfPath(root + "/" + reader_name));
+    reader.CreateIdAttr("UsdPrimvarReader_float2");
+
+    auto sampler = pxr::UsdShadeShader::Define(stage,)
 }
 
 
@@ -46,5 +79,18 @@ int main() {
     pxr::UsdModelAPI(root).SetKind(pxr::KindTokens->component);
 
     auto billboard = attach_billboard(stage, root.GetPath().GetString());
+    auto material = pxr::UsdShadeMaterial::Define(
+        stage,
+        pxr::SdfPath(billboard.GetPath().GetString() + "/" + "boardMat"));
+
+    auto material_path = material.GetPath().GetString();
+    auto shader = attach_surface_shader(
+        stage,
+        material,
+        material_path + "/" + "PBRShader"
+    );
+    auto reader = attach_texture(stage, shader, material_path);
+
+
     return 0;
 }
