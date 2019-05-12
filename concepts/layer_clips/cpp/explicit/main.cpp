3ee80397ea7d357fce0da7b1e75d22521487464d
Colin Kennedy <colinvfx@gmail.com>
2019-05-12 15:39:32 -0700

    Added layer_clips python and cpp folders

---
+++ concepts/layer_clips/cpp/explicit/main.cpp
@@ -0,0 +1,32 @@
+// IMPORT THIRD-PARTY LIBRARIES
+#include <pxr/usd/usd/clipsAPI.h>
+#include <pxr/usd/usd/stage.h>
+
+
+int main() {
+    auto stage = pxr::UsdStage::CreateInMemory();
+    stage->SetStartTimeCode(0);
+    stage->SetEndTimeCode(12);
+
+    auto prim = stage->DefinePrim(pxr::SdfPath("/Prim"));
+    auto model = pxr::UsdClipsAPI(prim);
+    model.SetClipActive({{0, 0}, {2, 1}});
+    model.SetClipAssetPaths(
+        {pxr::SdfAssetPath{"./clip_1.usda"}, pxr::SdfAssetPath{"./clip_2.usda"}}
+    );
+    model.SetClipPrimPath("/Clip");
+    model.SetClipTimes({{0, 0}, {1, 1}, {2, 0}, {3, 1}});
+    model.SetClipManifestAssetPath(pxr::SdfAssetPath{"./clip_manifest.usda"});
+
+    prim.GetReferences().AddReference(
+        "./ref.usda",
+        pxr::SdfPath{"/Ref"}
+    );
+
+    auto* result = new std::string();
+    stage->GetRootLayer()->ExportToString(result);
+    std::cout << *result << std::endl;
+    delete result;
+
+    return 0;
+}
---
+++ concepts/layer_clips/cpp/template_and_explicit/main.cpp
@@ -0,0 +1,44 @@
+// IMPORT STANDARD LIBRARIES
+#include <iostream>
+#include <string>
+
+// IMPORT THIRD-PARTY LIBRARIES
+#include <pxr/usd/usd/clipsAPI.h>
+#include <pxr/usd/sdf/types.h>
+#include <pxr/usd/usd/stage.h>
+
+
+int main() {
+    auto stage = pxr::UsdStage::CreateInMemory();
+    stage->SetStartTimeCode(0);
+    stage->SetEndTimeCode(2);
+
+    auto prim = stage->DefinePrim(pxr::SdfPath("/Set"));
+    std::string non_template_set_name {"non_template_clips"};
+    auto model = pxr::UsdClipsAPI(prim);
+    model.SetClipActive({{0.0, 0}}, non_template_set_name);
+    model.SetClipAssetPaths(
+        {pxr::SdfAssetPath{"./non_template_clip.usda"}},
+        non_template_set_name
+    );
+    model.SetClipPrimPath("/NonTemplate", non_template_set_name);
+
+    std::string template_set_name {"template_clips"};
+    model.SetClipTemplateAssetPath("./template_clip.##.usda", template_set_name);
+    model.SetClipTemplateEndTime(2);
+    model.SetClipTemplateStartTime(0);
+    model.SetClipTemplateStride(1, template_set_name);
+    model.SetClipPrimPath("/Template", template_set_name);
+
+    prim.GetReferences().AddReference(
+        "./ref.usda",
+        pxr::SdfPath{"/Ref"}
+    );
+
+    auto* result = new std::string();
+    stage->GetRootLayer()->ExportToString(result);
+    std::cout << *result << std::endl;
+    delete result;
+
+    return 0;
+}
