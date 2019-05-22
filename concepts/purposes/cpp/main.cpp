0a3e1b4ef8c28df0ce9e74631a163d4e0d596b28
Colin Kennedy <colinvfx@gmail.com>
2019-05-21 21:58:34 -0700

    Added 'purposes' folder

---
+++ concepts/purposes/cpp/main.cpp
@@ -0,0 +1,40 @@
+// IMPORT STANDARD LIBRARIES
+#include <string>
+
+// IMPORT THIRD-PARTY LIBRARIES
+#include "pxr/usd/sdf/path.h"
+#include "pxr/usd/usd/stage.h"
+#include "pxr/usd/usdGeom/cone.h"
+#include "pxr/usd/usdGeom/cube.h"
+#include "pxr/usd/usdGeom/cylinder.h"
+#include "pxr/usd/usdGeom/sphere.h"
+#include "pxr/usd/usdGeom/xform.h"
+
+
+int main() {
+    auto stage = pxr::UsdStage::CreateInMemory();
+    auto xform = pxr::UsdGeomXform::Define(stage, pxr::SdfPath("/Xform"));
+
+    auto cube = pxr::UsdGeomCube::Define(stage, pxr::SdfPath("/Xform/SomeGuide"));
+    auto purpose = cube.CreatePurposeAttr();
+    purpose.Set(pxr::UsdGeomTokens->guide);
+
+    auto sphere = pxr::UsdGeomSphere::Define(stage, pxr::SdfPath("/Xform/SomeRender"));
+    purpose = sphere.CreatePurposeAttr();
+    purpose.Set(pxr::UsdGeomTokens->render);
+
+    auto cone = pxr::UsdGeomCone::Define(stage, pxr::SdfPath("/Xform/SomeProxy"));
+    purpose = cone.CreatePurposeAttr();
+    purpose.Set(pxr::UsdGeomTokens->proxy);
+
+    auto cylinder = pxr::UsdGeomCylinder::Define(stage, pxr::SdfPath("/Xform/SomeDefault"));
+    purpose = cylinder.CreatePurposeAttr();
+    purpose.Set(pxr::UsdGeomTokens->default_);
+
+    auto* result = new std::string();
+    stage->ExportToString(result);
+    std::cout << *result << std::endl;
+    delete result;
+
+    return 0;
+}
---
+++ concepts/purposes/python/purpose.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# IMPORT THIRD-PARTY LIBRARIES
+from pxr import Usd
+from pxr import UsdGeom
+
+
+def main():
+    """Run the main execution of the current script."""
+    stage = Usd.Stage.CreateInMemory()
+    xform = UsdGeom.Xform(stage.DefinePrim('/Xform', 'Xform'))
+
+    cube = UsdGeom.Cube(stage.DefinePrim('/Xform/SomeGuide', 'Cube'))
+    purpose = cube.CreatePurposeAttr()
+    purpose.Set(UsdGeom.Tokens.guide)
+
+    sphere = UsdGeom.Sphere(stage.DefinePrim('/Xform/SomeRender', 'Sphere'))
+    purpose = sphere.CreatePurposeAttr()
+    purpose.Set(UsdGeom.Tokens.render)
+
+    cone = UsdGeom.Cone(stage.DefinePrim('/Xform/SomeProxy', 'Cone'))
+    purpose = cone.CreatePurposeAttr()
+    purpose.Set(UsdGeom.Tokens.proxy)
+
+    cylinder = UsdGeom.Cylinder(stage.DefinePrim('/Xform/SomeDefault', 'Cylinder'))
+    purpose = cylinder.CreatePurposeAttr()
+    purpose.Set(UsdGeom.Tokens.default_)
+
+    print(stage.GetRootLayer().ExportToString())
+
+
+if __name__ == "__main__":
+    main()
