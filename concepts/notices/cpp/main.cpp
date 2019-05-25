abf6e1c903126e11de654fa8b17e45e01f3d7f8d
Colin Kennedy <colinvfx@gmail.com>
2019-05-24 23:08:35 -0700

    WIP commit - Added 'notices/python'. 'notices/cpp' still needs work

---
+++ concepts/notices/cpp/main.cpp
@@ -0,0 +1,167 @@
+// IMPORT STANDARD LIBRARIES
+#include <iostream>
+
+// IMPORT THIRD-PARTY LIBRARIES
+#include "pxr/base/tf/diagnostic.h"
+#include "pxr/base/tf/stringUtils.h"
+#include "pxr/base/tf/weakBase.h"
+#include "pxr/usd/sdf/schema.h"
+#include "pxr/usd/usd/common.h"
+#include "pxr/usd/usd/notice.h"
+#include "pxr/usd/usd/stage.h"
+
+
+class UpdateNotice : public pxr::TfWeakBase
+{
+public:
+    UpdateNotice(const pxr::UsdStageWeakPtr &stage) {
+        pxr::TfNotice::Register(
+            pxr::TfCreateWeakPtr(this),
+            &UpdateNotice::_callback,
+            stage
+        );
+    }
+
+private:
+    void _callback(
+        const pxr::UsdNotice::ObjectsChanged &notice,
+        const pxr::UsdStageWeakPtr &sender
+    ) {
+        printf("The triggered sender %s", pxr::TfStringify(notice.GetStage()).c_str());
+        printf("Resynced paths [");
+        for (auto const &path : notice.GetResyncedPaths())
+        {
+            printf("%s, ", path.GetText());
+        }
+        printf("]\n");
+
+        printf("The path Prim that was affected [");
+        for (auto const &path : notice.GetChangedInfoOnlyPaths())
+        {
+            printf("%s, ", path.GetText());
+        }
+        printf("]\n");
+
+        std::cout
+            << "Affected object "
+            << notice.AffectedObject(sender->GetPrimAtPath(pxr::SdfPath("/SomeSphere")))
+            << '\n';
+
+        std::cout
+            << "Resynced? "
+            << notice.ResyncedObject(sender->GetPrimAtPath(pxr::SdfPath("/SomeSphere")))
+            << '\n';
+
+        std::cout
+            << "Changed? "
+            << notice.ChangedInfoOnly(sender->GetPrimAtPath(pxr::SdfPath("/SomeSphere")))
+            << '\n';
+    }
+};
+
+
+// class StageContentNoticeGlobal : public pxr::TfWeakBase
+// {
+// public:
+//     StageContentNoticeGlobal() {
+//         pxr::TfNotice::Register(
+//             pxr::TfCreateWeakPtr(this),
+//             &StageContentNoticeGlobal::_callback
+//         );
+//     }
+//
+// private:
+//     void _callback(
+//         const pxr::UsdNotice::StageContentsChanged &notice,
+//         const pxr::UsdStageWeakPtr &sender
+//     ) {
+//         // TODO : How do you print `notice`?
+//         printf("%s", pxr::TfStringify(sender).c_str());
+//     }
+// };
+
+
+// class StageTargetNoticeGlobal : public pxr::TfWeakBase
+// {
+// public:
+//     StageTargetNoticeGlobal() {
+//         pxr::TfNotice::Register(
+//             pxr::TfCreateWeakPtr(this),
+//             &StageTargetNoticeGlobal::_callback
+//         );
+//     }
+//
+// private:
+//     void _callback(
+//         const pxr::UsdNotice::StageEditTargetChanged &notice,
+//         const pxr::UsdStageWeakPtr &sender
+//     ) {
+//         // TODO : How do you print `notice`?
+//         printf("%s", pxr::TfStringify(sender).c_str());
+//     }
+// };
+//
+//
+// class ObjectNoticeGlobal : public pxr::TfWeakBase
+// {
+// public:
+//     ObjectNoticeGlobal() {
+//         pxr::TfNotice::Register(
+//             pxr::TfCreateWeakPtr(this),
+//             &ObjectNoticeGlobal::_callback
+//         );
+//     }
+//
+// private:
+//     void _callback(
+//         const pxr::UsdNotice::StageContentsChanged &notice,
+//         const pxr::UsdStageWeakPtr &sender
+//     ) {
+//         // TODO : How do you print `notice`?
+//         printf("%s", pxr::TfStringify(sender).c_str());
+//     }
+// };
+
+
+int main() {
+    auto stage = pxr::UsdStage::CreateInMemory();
+    std::cout << std::boolalpha;
+
+    {
+        // XXX : Once `updated` goes out of scope, the listener will stop listening
+        UpdateNotice updated {stage};
+        stage->DefinePrim(pxr::SdfPath {"/SomeSphere"});
+        stage->GetPrimAtPath(pxr::SdfPath {"/SomeSphere"}).SetMetadata(
+            pxr::SdfFieldKeys->Comment,
+            ""
+        );
+    }
+
+    stage->DefinePrim(pxr::SdfPath {"/SomeSphere"});
+    stage->GetPrimAtPath(pxr::SdfPath {"/SomeSphere"}).SetMetadata(
+        pxr::SdfFieldKeys->Comment,
+        ""
+    );
+
+    // XXX : Unlike Python, which has a `RegisterGlobally` function, C++ re-uses
+    // `Register` and uses a separate signature.
+    //
+    {
+        // StageContentNoticeGlobal contents;
+        // ObjectNoticeGlobal objects;
+        // StageTargetNoticeGlobal targets;
+
+        // stage->DefinePrim(pxr::SdfPath {"/Foo"});
+        // stage->SetEditTarget(stage->GetSessionLayer());
+        // stage->GetPrimAtPath(pxr::SdfPath {"/Foo"}).SetMetadata(
+        //     pxr::SdfFieldKeys->Comment,
+        //     ""
+        // );
+        // stage->GetPrimAtPath(pxr::SdfPath {"/Foo"}).SetMetadata(
+        //     pxr::SdfFieldKeys->Comment,
+        //     "x"
+        // );
+    }
+
+    return 0;
+}
---
+++ concepts/notices/python/notice.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# IMPORT FUTURE LIBRARIES
+from __future__ import print_function
+
+# IMPORT THIRD-PARTY LIBRARIES
+from pxr import Tf
+from pxr import Usd
+
+
+def update(notice, sender):
+    """Print example data that you can get from the callback."""
+    print('The triggered sender', notice.GetStage())
+    print('Resynced paths', notice.GetResyncedPaths())
+    print('The path Prim path that was affected', notice.GetChangedInfoOnlyPaths())
+    print('Affected object', notice.AffectedObject(sender.GetPrimAtPath("/SomeSphere")))
+    print('Resynced?', notice.ResyncedObject(sender.GetPrimAtPath("/SomeSphere")))
+    print('Changed?', notice.ChangedInfoOnly(sender.GetPrimAtPath("/SomeSphere")))
+
+
+def objects_changed(event, sender):
+    print(event, sender)
+
+
+def stage_changed(event, sender):
+    print(event, sender)
+
+
+def target_changed(event, sender):
+    print(event, sender)
+
+
+def main():
+    """Run the main execution of the current script."""
+    stage = Usd.Stage.CreateInMemory()
+
+    # XXX : You can register to a specific `pxr.Usd.Stage` Important:
+    # You must assign `Register` to a variable (even if you don't run
+    # `del` on it later) or the callback goes out of scope and does nothing.
+    #
+    updated = Tf.Notice.Register(Usd.Notice.ObjectsChanged, update, stage)
+    stage.DefinePrim('/SomeSphere')
+    stage.GetPrimAtPath("/SomeSphere").SetMetadata("comment", "")
+
+    # XXX : `del` revokes the notice
+    del updated
+    stage.DefinePrim('/SomeSphere2')
+    stage.GetPrimAtPath("/SomeSphere2").SetMetadata("comment", "")
+
+    # XXX : You can also register notices for the session (not to any specific stage)
+    # XXX : `pxr.Usd.Notice` comes with 3 types
+    #
+    # `Usd.Notice.ObjectsChanged`
+    # `Usd.Notice.StageContentsChanged`
+    # `Usd.Notice.StageEditTargetChanged`
+    #
+    contents = Tf.Notice.RegisterGlobally(
+        Usd.Notice.StageContentsChanged,
+        stage_changed,
+    )
+
+    objects = Tf.Notice.RegisterGlobally(
+        Usd.Notice.ObjectsChanged,
+        objects_changed,
+    )
+
+    targets = Tf.Notice.RegisterGlobally(
+        Usd.Notice.StageEditTargetChanged,
+        target_changed,
+    )
+
+    stage.DefinePrim("/Foo")
+    stage.SetEditTarget(stage.GetSessionLayer())
+    stage.GetPrimAtPath("/Foo").SetMetadata("comment", "")
+    stage.GetPrimAtPath("/Foo").SetMetadata("comment", "x")
+    print('Done')
+
+
+if __name__ == "__main__":
+    main()
