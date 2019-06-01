451aaec876ab263c8a39d23219a3042e9d08ccd9
Colin Kennedy <colinvfx@gmail.com>
2019-06-01 12:44:52 -0700

    Got using_contexts function to work

--- concepts/caching/cpp/main.cpp
+++ concepts/caching/cpp/main.cpp
@@ -12,15 +12,46 @@ void using_contexts() {
     pxr::UsdStageCache cache;
     std::cout << "Should be False (the cache was just created) " << cache.Contains(stage) << '\n';
 
+    pxr::UsdStageCache::Id saved_id;
     {
         pxr::UsdStageCacheContext context(cache);
         auto inner_stage = pxr::UsdStage::CreateInMemory();
         std::cout << "Has stage? " << cache.Contains(inner_stage) << '\n';
 
-        // {
-        //     auto blocked_context = pxr::UsdStageCacheContext();
-        // }
+        {
+            pxr::UsdStageCacheContext blocked_context(pxr::UsdBlockStageCachePopulation);
+            auto new_stage = pxr::UsdStage::CreateInMemory();
+            std::cout << "Has stage? (true) " << cache.Contains(inner_stage) << '\n';
+            std::cout << "Has new stage? (false) " << cache.Contains(new_stage) << '\n';
+        }
+
+        std::cout << "Still has stage? (true) " << cache.Contains(inner_stage) << '\n';
+        auto stage_id = cache.GetId(inner_stage);
+        std::cout << "The key that refers to the cached, opened USD stage " << stage_id.ToString() << '\n';
+        std::cout << "Found stage in cache " << (cache.Find(stage_id) == inner_stage) << '\n';
+
+        saved_id = cache.GetId(inner_stage);
     }
+
+    // XXX : A difference between C++ and Python, you cannot create a
+    // stage within a StageCacheContext automatically and then still
+    // retrieve it after the context exits because the scoping rules for
+    // C++ and Python are different
+    //
+    // To reflect this, I commented out the lines that work in Python
+    // but don't work here.
+    //
+    // std::cout << "Still has it?? " << cache.Contains(inner_stage) << '\n';
+    // cache.Clear();
+    // std::cout << "This value should be false now " << cache.Contains(inner_stage) << '\n';
+
+    // XXX : You can still refer to the stage from the cache. Just not
+    // with the `inner_stage` object
+    //
+    auto inner_stage = cache.Find(saved_id);
+    std::cout << "Still has it?? " << cache.Contains(inner_stage) << '\n';
+    cache.Clear();
+    std::cout << "This value should be false now " << cache.Contains(inner_stage) << '\n';
 }
 
 
