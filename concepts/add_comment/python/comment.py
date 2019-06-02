ecd9934ba1810b7dc7609658c77a5547867e21fe
Colin Kennedy <colinvfx@gmail.com>
2019-06-01 19:01:29 -0700

    add_comment - Ran 'black' on the code

--- concepts/add_comment/python/comment.py
+++ concepts/add_comment/python/comment.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""Show how to write a comment in USD Python API."""
+
 # IMPORT THIRD-PARTY LIBRARIES
-from pxr import Usd
-from pxr import UsdGeom
+from pxr import Usd, UsdGeom
 
 
 def main():
-    '''Run the main execution of the current script.'''
+    """Run the main execution of the current script."""
     stage = Usd.Stage.CreateInMemory()
 
     sphere = UsdGeom.Sphere(stage.DefinePrim("/SomeSphere", "Sphere"))
-    sphere.GetPrim().SetMetadata('comment', 'I am a comment')
+    sphere.GetPrim().SetMetadata("comment", "I am a comment")
 
     print(stage.GetRootLayer().ExportToString())
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
