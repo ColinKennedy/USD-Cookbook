3c3cc50a03e85af296f22d2ac7c7c8e3d8c4de5c
Colin Kennedy <colinvfx@gmail.com>
2019-06-06 21:46:06 -0700

    Got uniquify.py to work

--- concepts/uniquify_an_instance/python/uniquify.py
+++ concepts/uniquify_an_instance/python/uniquify.py
@@ -2,16 +2,42 @@
 # -*- coding: utf-8 -*-
 
 # IMPORT THIRD-PARTY LIBRARIES
-from pxr import Usd
+from pxr import Tf, Usd, UsdGeom
 
 
 def main():
     """Run the main execution of the current script."""
-    stage = Usd.Stage.Open('override.usda')
-    prim = stage.GetPrimAtPath('/AnotherPrim/InnerPrim1')
-    print(prim.IsInstanceable(), prim.IsInstance())
-    prim = stage.GetPrimAtPath('/MyPrim/SomeSphere')
-    print(prim.IsInstanceable(), prim.IsInstance())
+    stage = Usd.Stage.CreateInMemory()
+
+    master = "/MyPrim"
+
+    instances = ["/AnotherPrim/InnerPrim1", "/AnotherPrim/InnerPrim2"]
+    paths = ["/AnotherPrim", "/MyPrim/SomeSphere", master]
+
+    for path in paths + instances:
+        UsdGeom.Xform.Define(stage, path)
+
+    for instance in instances:
+        prim = stage.GetPrimAtPath(instance)
+        prim.GetReferences().AddReference(
+            stage.GetRootLayer().identifier, primPath="/MyPrim"
+        )
+        prim.SetInstanceable(True)
+        prim.SetTypeName("Sphere")
+
+    try:
+        prim = stage.DefinePrim("/AnotherPrim/InnerPrim1/SomePrimThatWillNotExist")
+    except Tf.ErrorException:
+        # XXX : Because "/AnotherPrim/InnerPrim1" is an instance,
+        # DefinePrim will raise an exception
+        #
+        print("EXCEPTION FOUND")
+
+    stage.DefinePrim("/AnotherPrim/InnerPrim1").SetInstanceable(False)
+    # XXX : We broke the instance so now it will not raise an exception
+    # If you want to, you can also do `if not prim.IsInstance(): stage.DefinePrim`
+    #
+    prim = stage.DefinePrim("/AnotherPrim/InnerPrim1/SomePrimThatWillNotExist")
 
 
 if __name__ == "__main__":
