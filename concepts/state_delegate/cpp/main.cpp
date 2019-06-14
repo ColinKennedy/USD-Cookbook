19dba1f4e6c095dd476d22cb556fcb094f32744f
Colin Kennedy <colinvfx@gmail.com>
2019-06-13 21:36:19 -0700

    WIP commit - Adding some external object for undo information

--- concepts/state_delegate/cpp/main.cpp
+++ concepts/state_delegate/cpp/main.cpp
@@ -13,35 +13,62 @@
 
 using Record = std::function<bool()>;
 
-
-class StateSingleton {
+class S
+{
     public:
-        void operator=(StateSingleton const&) = delete;
-
-        static StateSingleton &get() {
-            static StateSingleton instance;
+        static S& getInstance()
+        {
+            static S    instance; // Guaranteed to be destroyed.
+                                  // Instantiated on first use.
             return instance;
         }
+    private:
+        S() {}                    // Constructor? (the {} brackets) are needed here.
 
-        void push_back(Record const &record) {
-            this->memos.push_back(record);
+    public:
+        S(S const&)               = delete;
+        void operator=(S const&)  = delete;
+
+        // Note: Scott Meyers mentions in his Effective Modern
+        //       C++ book, that deleted functions should generally
+        //       be public as it results in better error messages
+        //       due to the compilers behavior to check accessibility
+        //       before deleted status
+};
+
+template<typename HeirT>
+class Singleton {
+public:
+    Singleton() = delete;
+    Singleton(const Singleton &) = delete;
+    Singleton &operator=(const Singleton &) = delete;
+
+    static HeirT &instance() {
+        static HeirT instance;
+        return instance;
+    }
+};
+
+
+class StateSingleton : public Singleton<StateSingleton> {
+public:
+    void push_back(Record const &record) {
+        this->memos.push_back(record);
+    }
+
+    bool undo() {
+        if (this->memos.empty()) {
+            std::cout << "The undo queue is empty. There's nothing to undo.\n";
+            return false;
         }
 
-        bool undo() {
-            if (this->memos.empty()) {
-                std::cout << "The undo queue is empty. There's nothing to undo.\n";
-                return false;
-            }
+        auto function = this->memos.back();
+        bool result = function();
+        this->memos.pop_back();
+        return result;
+    };
 
-            auto function = this->memos.back();
-            bool result = function();
-            this->memos.pop_back();
-            return result;
-        };
-
-    private:
-        StateSingleton() {}
-        std::vector<Record> memos;
+    std::vector<Record> memos;
 };
 
 
@@ -50,41 +77,40 @@ TF_DECLARE_WEAK_AND_REF_PTRS(StateDelegate);
 
 
 class StateDelegate : public pxr::SdfSimpleLayerStateDelegate {
-    public:
-        static pxr::StateDelegateRefPtr New();
+public:
+    static pxr::StateDelegateRefPtr New();
+    StateDelegate() : state(StateSingleton::instance()) {
+    }
 
-    protected:
-        bool _InvertCreateSpec(const pxr::SdfPath& path, bool inert) {
-            std::cout << "Undoing" << path << '\n';
+protected:
+    bool _InvertCreateSpec(const pxr::SdfPath& path, bool inert) {
+        std::cout << "Undoing" << path << '\n';
 
-            if (!this->_GetLayer()) {
-                std::cout << "Could not undo because the layer doesn't exist\n";
-                // XXX : Always check that the layer exists before modifying it
-                return false;
-            }
-
-            this->DeleteSpec(path, inert);
-            return true;
+        if (!this->_GetLayer()) {
+            std::cout << "Could not undo because the layer doesn't exist\n";
+            // XXX : Always check that the layer exists before modifying it
+            return false;
         }
 
-        virtual void _OnCreateSpec(const pxr::SdfPath &path, pxr::SdfSpecType type, bool inert) override {
-            std::cout << "Spec was created\n";
-            this->state.push_back(
-                std::bind(
-                    &StateDelegate::_InvertCreateSpec,
-                    this,
-                    path,
-                    inert
-                )
-            );
-        }
-        virtual void _OnSetLayer(const pxr::SdfLayerHandle &layer) override {
-            std::cout << "Layer was associated with this object\n";
-        }
+        this->DeleteSpec(path, inert);
+        return true;
+    }
 
-    private:
-        typedef pxr::SdfSimpleLayerStateDelegate super;
-        StateSingleton state = StateSingleton::get();
+    virtual void _OnCreateSpec(const pxr::SdfPath &path, pxr::SdfSpecType type, bool inert) override {
+        std::cout << "Spec was created\n";
+        this->state.push_back(
+            std::bind(&StateDelegate::_InvertCreateSpec, this, path, inert
+        ));
+        std::cout << this->state.memos.empty();
+    }
+
+    virtual void _OnSetLayer(const pxr::SdfLayerHandle &layer) override {
+        std::cout << "Layer was associated with this object\n";
+    }
+
+private:
+    typedef pxr::SdfSimpleLayerStateDelegate super;
+    StateSingleton state;
 };
 PXR_NAMESPACE_CLOSE_SCOPE
 
@@ -111,6 +137,7 @@ int main() {
     std::cout << "Is dirty " << root->GetStateDelegate()->IsDirty() << '\n';
 
     auto state = StateSingleton::get();
+    std::cout << state.memos.empty();
     state.undo();
     state.undo();
     state.undo();
