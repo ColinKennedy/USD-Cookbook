684282d46e5b3377d5c849507b7b289712a02fb3
Colin Kennedy <colinvfx@gmail.com>
2019-08-29 01:07:53 -0700

    Changed __packageinit__.py to have a more detailed message

--- tricks/custom_schema_with_python_bindings/compiling_the_schema/src/__packageinit__.py
+++ tricks/custom_schema_with_python_bindings/compiling_the_schema/src/__packageinit__.py
@@ -1,1 +1,8 @@
-""""""""
+"""The main testout custom schema.
+
+This dynamic library was created based on USD's custom schema classes tutorial.
+
+Reference:
+    https://graphics.pixar.com/usd/docs/Generating-New-Schema-Classes.html
+
+"""
