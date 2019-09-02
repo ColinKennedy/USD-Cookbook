903bcf67a01c77c94e72f507b56859ccd20d123d
Colin Kennedy <colinvfx@gmail.com>
2019-09-02 12:01:29 -0700

    Added basic usd_resolve_info C++ project

---
+++ concepts/usd_resolve_info/cpp/main.cpp
@@ -0,0 +1,56 @@
+// IMPORT STANDARD LIBRARIES
+#include <iostream>
+
+// IMPORT THIRD-PARTY LIBRARIES
+#include <pxr/base/tf/token.h>
+#include <pxr/usd/sdf/path.h>
+#include <pxr/usd/usd/attribute.h>
+#include <pxr/usd/usd/stage.h>
+
+
+int main() {
+    auto stage = pxr::UsdStage::Open("../usd_resolve_info.usda");
+    auto prim = stage->GetPrimAtPath(pxr::SdfPath{"/SomePrim"});
+
+    std::cout <<
+        "value_clipped_property - Is value clip \"" <<
+            (stage->GetPrimAtPath(pxr::SdfPath{"/PrimWithValueClips"})
+            .GetAttribute(pxr::TfToken{"value_clipped_property"})
+            .GetResolveInfo(1)
+            .GetSource()
+            == pxr::UsdResolveInfoSourceValueClips)
+        << "\".\n";
+
+    std::cout <<
+        "time_samples_property - Is time samples \"" <<
+        (
+            prim.GetAttribute(pxr::TfToken{"time_samples_property"}).GetResolveInfo().GetSource()
+            == pxr::UsdResolveInfoSourceTimeSamples
+        )
+        << "\".\n";
+
+    std::cout <<
+        "default_property - Is a default value \"" << (
+            prim.GetAttribute(pxr::TfToken{"default_property"}).GetResolveInfo().GetSource()
+            == pxr::UsdResolveInfoSourceDefault
+        )
+        << "\".\n";
+
+    auto sphere = stage->GetPrimAtPath(pxr::SdfPath{"/SomeSphere"});
+
+    std::cout <<
+        "radius - Is a type fallback value \"" << (
+            sphere.GetAttribute(pxr::TfToken{"radius"}).GetResolveInfo().GetSource()
+            == pxr::UsdResolveInfoSourceFallback
+        )
+    << "\".\n";
+
+    std::cout <<
+        "xformOpOrder - Is empty value \"" << (
+            prim.GetAttribute(pxr::TfToken{"xformOpOrder"}).GetResolveInfo().GetSource()
+            == pxr::UsdResolveInfoSourceNone
+        )
+    << "\".\n";
+
+    return 0;
+}
