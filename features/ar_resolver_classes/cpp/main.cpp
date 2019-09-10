4c97cc5ceaa53386f5b714bf682f577dc0f2c611
Colin Kennedy <colinvfx@gmail.com>
2019-09-09 23:03:46 -0700

    Added more print statements

--- features/ar_resolver_classes/cpp/main.cpp
+++ features/ar_resolver_classes/cpp/main.cpp
@@ -6,6 +6,7 @@
 #include <pxr/usd/ar/resolver.h>
 #include <pxr/usd/ar/resolverContextBinder.h>
 #include <pxr/usd/usd/stage.h>
+#include <pxr/usd/usdGeom/sphere.h>
 #include <pxr/usd/usdUtils/dependencies.h>
 
 
@@ -45,6 +46,24 @@ int main() {
         std::cout << '\n';
         stage = pxr::UsdStage::Open("some_stage.usda");
     }
+
+    std::cout << '\n';
+    std::cout
+        <<
+            "XXX: But if we try to query information from the paths, that "
+            "doesn't work. It's the same whether we're inside or outside "
+            "of the context.\n";
+    auto prim = pxr::UsdGeomSphere{stage->GetPrimAtPath(pxr::SdfPath{"/SomePrim"})};
+    double radius;
+    prim.GetRadiusAttr().Get(&radius);
+    std::cout << radius << '\n';
+
+    prim = pxr::UsdGeomSphere{stage->GetPrimAtPath(pxr::SdfPath{"/SomePrim2"})};
+    prim.GetRadiusAttr().Get(&radius);
+    std::cout << radius << '\n';
+    prim = pxr::UsdGeomSphere{stage->GetPrimAtPath(pxr::SdfPath{"/SomePrim3"})};
+    prim.GetRadiusAttr().Get(&radius);
+    std::cout << "This will be None, because @some_nested_layer.usda will not resolve: \"" << radius << "\".\n" << '\n';
 
     return 0;
 }
