2756331de369a1ec7552272b51b6d92224fddd3c
Colin Kennedy <colinvfx@gmail.com>
2019-11-11 12:20:34 -0800

    Added module.cpp and moduleDeps.cpp

---
+++ features/imaging_adapter/star_gprim/src/module.cpp
@@ -0,0 +1,11 @@
+#include "Python.h"
+#include "pxr/pxr.h"
+#include "pxr/base/tf/pyModule.h"
+
+PXR_NAMESPACE_USING_DIRECTIVE
+
+TF_WRAP_MODULE
+{
+    TF_WRAP(UsdStarStar);
+    TF_WRAP(UsdStarTokens);
+}
