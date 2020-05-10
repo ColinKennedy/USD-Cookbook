8ee4ef8b20e68705bd302887310c34dddfefba51
Colin Kennedy <colinvfx@gmail.com>
2020-05-10 12:34:15 -0700

    Got C++ code to work

--- features/edit_targets/cpp/main.cpp
+++ features/edit_targets/cpp/main.cpp
@@ -20,43 +20,43 @@ pxr::UsdStageRefPtr _make_target()
 
 int main() {
     auto inner_stage = _make_target();
-    // auto main_stage = pxr::UsdStage::CreateInMemory();
+    auto main_stage = pxr::UsdStage::CreateInMemory();
+
+    // XXX : In order to use `inner_stage` in an EditContext, it must be
+    // in `main_stage`'s local LayerStack (e.g. it must be a sublayer)
     //
-    // // XXX : In order to use `inner_stage` in an EditContext, it must be
-    // // in `main_stage`'s local LayerStack (e.g. it must be a sublayer)
-    // //
-    // main_stage->GetRootLayer()->GetSubLayerPaths().push_back(
-    //     inner_stage->GetRootLayer()->GetIdentifier()
-    // );
-    //
-    // auto* result = new std::string();
-    // main_stage->GetRootLayer()->ExportToString(result);
-    // std::cout << *result << std::endl;
-    //
-    // std::cout << "Inner stage before context\n";
-    // inner_stage->GetRootLayer()->ExportToString(result);
-    // std::cout << *result << std::endl;
-    //
-    // {
-    //     pxr::UsdEditContext context {main_stage, inner_stage->GetRootLayer()};
-    //     auto sphere = pxr::UsdGeomSphere(main_stage->GetPrimAtPath(pxr::SdfPath{"/root/sphere"}));
-    //     sphere.GetRadiusAttr().Set(10);
-    // }
-    //
-    // std::cout << "Inner stage after context\n";
-    // inner_stage->GetRootLayer()->ExportToString(result);
-    // std::cout << *result << std::endl;
-    //
-    // main_stage->SetEditTarget(pxr::UsdEditTarget(inner_stage->GetRootLayer()));
-    // auto sphere = pxr::UsdGeomSphere(main_stage->GetPrimAtPath(pxr::SdfPath{"/root/sphere"}));
-    // sphere.GetRadiusAttr().Set(5);
-    //
-    // std::cout << "Inner stage after setting\n";
-    // inner_stage->GetRootLayer()->ExportToString(result);
-    // std::cout << *result << std::endl;
-    //
-    // delete result;
-    // result = nullptr;
+    main_stage->GetRootLayer()->GetSubLayerPaths().push_back(
+        inner_stage->GetRootLayer()->GetIdentifier()
+    );
+
+    auto* result = new std::string();
+    main_stage->GetRootLayer()->ExportToString(result);
+    std::cout << *result << std::endl;
+
+    std::cout << "Inner stage before context\n";
+    inner_stage->GetRootLayer()->ExportToString(result);
+    std::cout << *result << std::endl;
+
+    {
+        pxr::UsdEditContext context {main_stage, inner_stage->GetRootLayer()};
+        auto sphere = pxr::UsdGeomSphere(main_stage->GetPrimAtPath(pxr::SdfPath{"/root/sphere"}));
+        sphere.GetRadiusAttr().Set(10.0);
+    }
+
+    std::cout << "Inner stage after context\n";
+    inner_stage->GetRootLayer()->ExportToString(result);
+    std::cout << *result << std::endl;
+
+    main_stage->SetEditTarget(pxr::UsdEditTarget(inner_stage->GetRootLayer()));
+    auto sphere = pxr::UsdGeomSphere(main_stage->GetPrimAtPath(pxr::SdfPath{"/root/sphere"}));
+    sphere.GetRadiusAttr().Set(5.0);
+
+    std::cout << "Inner stage after setting\n";
+    inner_stage->GetRootLayer()->ExportToString(result);
+    std::cout << *result << std::endl;
+
+    delete result;
+    result = nullptr;
 
     return 0;
 }
