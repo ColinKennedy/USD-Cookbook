3bce98000630ec08923a851a31a4ca8ff1c4981a
Colin Kennedy <colinvfx@gmail.com>
2020-05-10 11:58:07 -0700

    Adding basic edit_targets folder

---
+++ features/edit_targets/README.md
@@ -0,0 +1,10 @@
+# Quick Reference
+
+
+## cpp
+
+
+# References
+[Edit Target Glossary Definition](https://graphics.pixar.com/usd/docs/USD-Glossary.html#USDGlossary-EditTarget)
+
+[UsdEditTarget Documentation](https://graphics.pixar.com/usd/docs/api/class_usd_edit_target.html)
---
+++ features/edit_targets/python/edit.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pxr import Usd, UsdGeom
+
+
+def _make_target():
+    stage = Usd.Stage.CreateInMemory()
+    root = UsdGeom.Scope.Define(stage, "/root")
+    UsdGeom.Sphere.Define(stage, root.GetPath().AppendChild("sphere"))
+    stage.SetDefaultPrim(root.GetPrim())
+
+    return stage
+
+
+def main():
+    """Run the main execution of the current script."""
+    inner_stage = _make_target()
+    main_stage = Usd.Stage.CreateInMemory()
+
+    # XXX : In order to use `inner_stage` in an EditContext, it must be
+    # in `main_stage`'s local LayerStack (e.g. it must be a sublayer)
+    #
+    main_stage.GetRootLayer().subLayerPaths.append(inner_stage.GetRootLayer().identifier)
+
+    print(main_stage.GetRootLayer().ExportToString())
+    print('Inner stage before context')
+    print(inner_stage.GetRootLayer().ExportToString())
+
+    with Usd.EditContext(main_stage, inner_stage.GetRootLayer()):
+        sphere = UsdGeom.Sphere(main_stage.GetPrimAtPath("/root/sphere"))
+        sphere.GetRadiusAttr().Set(10)
+
+    print('Inner stage after context')
+    print(inner_stage.GetRootLayer().ExportToString())
+
+    main_stage.SetEditTarget(Usd.EditTarget(inner_stage.GetRootLayer()))
+
+    sphere = UsdGeom.Sphere(main_stage.GetPrimAtPath("/root/sphere"))
+    sphere.GetRadiusAttr().Set(5)
+
+    print('Inner stage after setting')
+    print(inner_stage.GetRootLayer().ExportToString())
+
+
+if __name__ == "__main__":
+    main()
